9e0dc4d6298c0a7a4e7dfefee1d9a0c3b22be548
Owen Rodriguez <owenzeledon@icloud.com>
2024-08-10 12:56:14 -0400

    feat: implementation of header and total chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const home = () => {
-  return <div>home</div>;
+  const loggedIn = { firstName: "Owen" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your transactions."
+          />
+        </header>
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.43}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default home;
